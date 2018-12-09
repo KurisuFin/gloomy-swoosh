d436b886cc5d6f8a37d32f96e5e4a190554a45db
Kristian Pulkkinen <kurisu@iki.fi>
2018-12-09 18:06:45 +0200

    Imagecam for testing. Refreshing gui works

--- main.py
+++ main.py
@@ -1,100 +1,71 @@
-from time import sleep
-from tkinter import *
-from PIL import Image, ImageTk
 import numpy as np
 import cv2
-
-import threading
-from threading import Thread
-from time import sleep
-
-
-# def asdf():
-#     cap = cv2.VideoCapture(0)
-#
-#     while True:
-#         ret, frame = cap.read()
-#         cv2.imwrite('path/to/image.png', frame)
-#         if cv2.waitKey(30) & 0xFF == ord('q'):
-#             break
-#
-#     cap.release()
-#     cv2.destroyAllWindows()
+from tkinter import *
+from PIL import Image, ImageTk, ImageDraw
+from datetime import datetime
+from math import floor
+from time import time
 
 
-#
+class Camera(object):
+    @staticmethod
+    def _set_time(img, fill=(0, 0, 0)):
+        t = datetime.today().strftime('%d.%m.%Y  %H:%M:%S.%f')
+        d = ImageDraw.Draw(img)
+        d.text((10, 10), t, fill=fill)
+
+    def take(self):
+        raise NotImplementedError
+
+    def release(self):
+        raise NotImplementedError
 
 
-class Capturer(object):
+class WebCamera(Camera):
     def __init__(self, cam_index):
         self.cap = cv2.VideoCapture(cam_index)
 
     def take(self):
-        if self.cap.isOpened():
-            _, cv2_img = self.cap.read()
-            print(type(cv2_img))
-            # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 100)
-            # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 100)
-            cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-            pil_img = Image.fromarray(cv2_img)
-            return pil_img
-        else:
-            print('Problem with video capturer')
+        self.cap.grab()
+        _, cv2_img = self.cap.retrieve()
+        # print(dir(self.cap))
+
+        cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(cv2_img)
+        self._set_time(img, fill=(255, 255, 0))
+        return ImageTk.PhotoImage(img)
 
     def release(self):
         self.cap.release()
         cv2.destroyAllWindows()
+        print('WebCamera released')
 
 
-capturer = Capturer(1)
-root = Tk()
+class ImageCamera(Camera):
+    def take(self):
+        img = Image.new('RGB', (640, 480), color=(255, 255, 255))
+        self._set_time(img)
+        return ImageTk.PhotoImage(image=img)
 
-img = ImageTk.PhotoImage(image=capturer.take())
-
-label = Label(root, relief=RAISED, image=img)
-
-
-def callback(e):
-    change_image()
-    # img2 = ImageTk.PhotoImage(capturer.take())
-    # label.configure(image=img2)
-    # label.image = img2
+    def release(self):
+        pass
 
 
-def change_image():
-        capturer.cap.grab()
-        _, cv2_img = capturer.cap.retrieve()
+if __name__ == "__main__":
+    def refresher():
+        img = camera.take()
+        label.configure(image=img)
+        label.image = img
+        root.after(100, refresher)
 
-        cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-        pil_img = Image.fromarray(cv2_img)
+    root = Tk()
 
-        img2 = ImageTk.PhotoImage(pil_img)
-        label.configure(image=img2)
-        label.image = img2
+    camera = WebCamera(0)
+    # camera = ImageCamera()
+    label = Label(root, relief=RAISED)
+    label.pack(side='top')
 
+    refresher()
+    root.mainloop()
+    camera.release()
 
-def threaded_function(i, x):
-    for i in range(i):
-        capturer.cap.grab()
-        w = cv2.waitKey(10)
-        print(w)
-        if w:
-            _, cv_img = capturer.cap.retrieve()
-            img2 = ImageTk.PhotoImage()
-            label.configure(image=img2)
-            label.image = img2
-
-        print("running %s" % x)
-        sleep(1)
-
-
-# thread = Thread(target=threaded_function, kwargs={'i': 10, 'x': 'yksi'})
-# thread.start()
-
-
-label.pack(side='top')
-root.bind("<Return>", callback)
-
-root.mainloop()
-
-exit(0)
